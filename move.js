1239ef61b19a454513ab0b810a935fcf557e5926
Lauren Suguro <lsuguro@Laurens-MacBook-Pro.local>
2023-03-20 17:02:32 -0400

    character can move with arrow keys

--- move.js
+++ move.js
@@ -1,12 +1,59 @@
 function move(element) {
-    element.style.position = 'fixed'
+  element.style.position = "fixed";
 
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
-    }
+  function moveToCoordinates(left, bottom) {
+    element.style.left = left + "px";
+    element.style.bottom = bottom + "px";
+  }
 
-    return {
-        to: moveToCoordinates
-    }
+  function moveWithArrowKeys(left, bottom) {
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    element.style.left = x + "px";
+    element.style.bottom = y + "px";
+
+    setInterval(function () {
+      if (direction === "west") {
+        x = x - 1;
+      }
+      if (direction === "north") {
+        y = y + 1;
+      }
+      if (direction === "east") {
+        x = x + 1;
+      }
+      if (direction === "south") {
+        y = y - 1;
+      }
+      element.style.left = x + "px";
+      element.style.bottom = y + "px";
+    }, 1);
+
+    //use keyboard arrows to change directions
+    document.addEventListener("keydown", function (e) {
+      if (e.repeat) return;
+      if (e.key === "ArrowLeft") {
+        direction = "west";
+      }
+      if (e.key === "ArrowUp") {
+        direction = "north";
+      }
+      if (e.key === "ArrowRight") {
+        direction = "east";
+      }
+      if (e.key === "ArrowDown") {
+        direction = "south";
+      }
+    });
+
+    document.addEventListener("keyup", function (e) {
+      direction = null;
+    });
+  }
+  return {
+    to: moveToCoordinates,
+    withArrowKeys: moveWithArrowKeys,
+  };
 }
